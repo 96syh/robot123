e82d1da06e101c8a78b586c2a28be3e1f24a332a
beautiful ZFQ <2268985483@qq.com>
2024-04-01 14:47:13 +0800

    赵芳琦

---
+++ 1-211008114-赵芳琦.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+#define ZFQ 3.14
+
+using namespace std;
+
+void shixun1(double* x, double* y, double a, double b, double k) {
+    double c = *x;
+    double d = *y;
+    double e;
+    double f;
+    double h;
+    double theta;
+
+    h = atan(k);
+    theta = ZFQ / 2 - h;
+    e = (c) * cos(theta) + sin(theta) * (d) + (a) * cos(theta) + sin(theta) * (b) - a;
+    f = -(c) * sin(theta) + cos(theta) * (d) - (a) * sin(theta) + cos(theta) * (b) - b;
+    *x = e;
+    *y = f;
+}
+
+int main() {
+    double x = 10;
+    double y = 10;
+    double a = 10;
+    double b = 25;
+    double x1 = 30;
+    double y1 = 10;
+    double x2 = 20;
+    double y2 = 25;
+    double k = 2;
+
+    shixun1(&x, &y, a, b, k);
+    shixun1(&x1, &y1, a, b, k);
+    shixun1(&x2, &y2, a, b, k);
+
+    cout << "1: (" << x << ", " << y << ")" << endl;
+    cout << "2: (" << x1 << ", " << y1 << ")" << endl;
+    cout << "3: (" << x2 << ", " << y2 << ")" << endl;
+
+    return 0;
+}
