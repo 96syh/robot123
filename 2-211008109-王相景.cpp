9d3d7f2540e5cebb478d6812af3c1e9e839b806e
wangxjwj <2775278375@qq.com>
2024-04-01 14:23:45 +0800

    wxj

---
+++ 2-211008109-王相景.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#define wangxiangjing 3.14
+void shuzu(double* x, double* y, double k, double a, double b, double* resultx, double* resulty) {
+
+    double c;
+    double o;
+
+    c = atan(k);
+    o = wangxiangjing / 2 - c;
+
+    for (int i = 0; i < 3; i++) {
+        resultx[i] = cos(o) * x[i] + sin(o) * y[i] + cos(o) * a + sin(o) * b - a;
+        resulty[i] = -sin(o) * x[i] + cos(o) * y[i] - sin(o) * a + cos(o) * b - b;
+    }
+
+
+}
+int main() {
+    double x[3] = { 10, 30, 20 };
+    double y[3] = { 10, 10, 25 };
+    double a = 0;
+    double b = 1;
+    double k = 2;
+    double resultx[3] = { 0 };
+    double resulty[3] = { 0 };
+    shuzu(x, y, k, a, b, resultx, resulty);
+    for (int i = 0; i < 3; i++) {
+        cout << "wxj" << resultx[i] << endl;
+        cout << "wxj" << resulty[i] << endl;
+    }
+    return 0;
+}
