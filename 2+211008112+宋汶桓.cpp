c7dfc76b5a09665299c194f238400786f702f7fb
swhsss <161422441+swhsss@users.noreply.github.com>
2024-04-01 16:32:17 +0800

    宋汶桓

---
+++ 2+211008112+宋汶桓.cpp
@@ -0,0 +1,43 @@
+
+
+#include <iostream>
+#include <corecrt_math.h>
+using namespace std;
+#define songwenhuan 3.14
+
+void XZ(double* x, double* y, double a, double b, double k, double* resultx, double* resulty) {
+
+    double theta = songwenhuan;
+    theta = songwenhuan / 2 - atan(k);
+    for (int i = 0; i < 3; i++) {
+        resultx[i] = cos(theta) * x[i] - sin(theta) * y[i] + a;
+        resulty[i] = sin(theta) * x[i] + cos(theta) * y[i] + b;
+    }
+}
+int main() {
+    double x[3] = { 0 };
+    double y[3] = { 0 };
+    x[0] = 10;
+    y[0] = 10;
+
+    x[1] = 30;
+    y[1] = 10;
+
+    x[2] = 20;
+    y[2] = 25;
+
+    double a = 1;
+    double b = 1;
+    double k = 2;
+    double resultx[3] = { 0 };
+    double resulty[3] = { 0 };
+    XZ(x, y, a, b, k, resultx, resulty);
+    for (int i = 0; i < 3; i++) {
+
+        cout << "SWH" << resultx[i] << endl;
+        cout << "SWH" << resulty[i] << endl;
+    }
+    return 0;
+}
+
+
