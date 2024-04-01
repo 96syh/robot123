1598e5fc5d5c2c04f4d35b58edda38030062c86f
jiangdonglai <2485507689@qq.com>
2024-04-01 15:05:44 +0800

    2-211008131-姜冬来

---
+++ 2-211008131-姜冬来.cpp
@@ -0,0 +1,33 @@
+#include<math.h>
+#include<iostream>
+using namespace std;
+#define jiangdonglai 3.14;
+void WW(double* x, double* y, double a, double b, double k, double* resultx, double* resulty) {
+	double s = jiangdonglai;
+	s = s / 2;
+	for (int i = 0; i < 3; i++) {
+		resultx[i] = x[i] * cos(s - atan(k)) - y[i] * sin(s - atan(k)) - a * cos(s - atan(k)) + b * sin(s - atan(k)) + a;
+		resulty[i] = x[i] * sin(s - atan(k)) + y[i] * cos(s - atan(k)) - a * sin(s - atan(k)) - b * cos(s - atan(k)) + b;
+	}
+}
+int main() {
+	double x[3] = { 0 };
+	double y[3] = { 0 };
+	x[0] = 10;
+	y[0] = 10;
+	x[1] = 30;
+	y[1] = 10;
+	x[2] = 20;
+	y[2] = 25;
+	double a = 0;
+	double b = 1;
+	double k = 2;
+	double resultx[3] = { 0 };
+	double resulty[3] = { 0 };
+	WW(x, y, a, b, k, resultx, resulty);
+	for (int i = 0; i < 3; i++) {
+		cout << "jiangdonglai" << resultx[i] << endl;
+		cout << "jiangdonglai" << resulty[i] << endl;
+	}
+	return 0;
+}
