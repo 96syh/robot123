2736e988628ad47b8c4686b57920f8b03757308f
jiangdonglai <2485507689@qq.com>
2024-04-01 15:02:14 +0800

    1-211008131-姜冬来

---
+++ 1-211008131-姜冬来.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#define jiangdonglai 3.14
+
+void xuanzhuan(double* x, double* y, double theta, double a, double b) {
+    double c = *x;
+    double d = *y;
+    double e;
+    double f;
+    e = cos(theta) * c - sin(theta) * d + cos(theta) * a - sin(theta) * b - a;
+    f = sin(theta) * c + cos(theta) * d + sin(theta) * a + cos(theta) * b - b;
+    *x = e;
+    *y = f;
+}
+
+int main() {
+    double x = 10;
+    double y = 10;
+    double x1 = 30;
+    double y1 = 10;
+    double x2 = 20;
+    double y2 = 25;
+    double a = 10;
+    double b = 25;
+    double theta = jiangdonglai;
+    theta = theta / 6;
+    xuanzhuan(&x, &y, theta, a, b);
+    xuanzhuan(&x1, &y1, theta, a, b);
+    xuanzhuan(&x2, &y2, theta, a, b);
+    cout << "Point 1: (" << x << ", " << y << ")" << endl;
+    cout << "Point 2: (" << x1 << ", " << y1 << ")" << endl;
+    cout << "Point 3: (" << x2 << ", " << y2 << ")" << endl;
+    cout << "Qµã: (" << a << ", " << b << ")" << endl;
+    return 0;
+}
