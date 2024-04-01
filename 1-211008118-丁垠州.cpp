f5ada1162f760fc9d4fe0cfddbd86fddca96bfec
huanxidyz <163818553+huanxidyz@users.noreply.github.com>
2024-04-01 16:24:54 +0800

    丁垠州

---
+++ 1-211008118-丁垠州.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.9.34616.47
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Project5", "Project5\Project5.vcxproj", "{CC627E0F-3B27-424A-B521-8976230FBCF4}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{CC627E0F-3B27-424A-B521-8976230FBCF4}.Debug|x64.ActiveCfg = Debug|x64
+		{CC627E0F-3B27-424A-B521-8976230FBCF4}.Debug|x64.Build.0 = Debug|x64
+		{CC627E0F-3B27-424A-B521-8976230FBCF4}.Debug|x86.ActiveCfg = Debug|Win32
+		{CC627E0F-3B27-424A-B521-8976230FBCF4}.Debug|x86.Build.0 = Debug|Win32
+		{CC627E0F-3B27-424A-B521-8976230FBCF4}.Release|x64.ActiveCfg = Release|x64
+		{CC627E0F-3B27-424A-B521-8976230FBCF4}.Release|x64.Build.0 = Release|x64
+		{CC627E0F-3B27-424A-B521-8976230FBCF4}.Release|x86.ActiveCfg = Release|Win32
+		{CC627E0F-3B27-424A-B521-8976230FBCF4}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {4E774A06-5600-44FD-A8E4-2BF7D3EB2E2F}
+	EndGlobalSection
+EndGlobal
