df7da310e7a36379fa53ce34daa70bed6476aa5c
xiaoaoxiangqianchong <614232946@qq.com>
2024-04-01 15:25:28 +0800

    任铄澳

---
+++ 2-211008130-任铄澳.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#define renshuoao 3.14
+void sz(double* x, double* y, double k, double a, double b, double* resultx, double* resulty) {
+    double c;
+    double o;
+    c = atan(k);
+    o = renshuoao / 2 - c;
+    for (int i = 0; i < 3; i++) {
+        resultx[i] = cos(o) * x[i] + sin(o) * y[i] + cos(o) * a + sin(o) * b - a;
+        resulty[i] = -sin(o) * x[i] + cos(o) * y[i] - sin(o) * a + cos(o) * b - b;
+    }
+}
+int main() {
+    double x[3] = { 10, 30, 20 };
+    double y[3] = { 10, 10, 25 };
+    double a = 0;
+    double b = 1;
+    double k = 2;
+    double resultx[3] = { 0 };
+    double resulty[3] = { 0 };
+    sz(x, y, k, a, b, resultx, resulty);
+    for (int i = 0; i < 3; i++) {
+        cout << "rsa" << resultx[i] << endl;
+        cout << "rsa" << resulty[i] << endl;
+    }
